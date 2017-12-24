f6b68c421c95ccc701531374c2f874370ab53bee
Roman Ilin <ilrose887@gmail.com>
2017-12-24 17:54:34 +0800

    data-container created

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ильин Роман on 24.12.2017.
+//  Copyright © 2017 Romax. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
