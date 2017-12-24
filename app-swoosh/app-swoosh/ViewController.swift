46e415eacbcdde8297c96274dbf9c495bcafc669
Roman Ilin <ilrose887@gmail.com>
2017-12-24 15:55:39 +0800

    back button added

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -18,15 +18,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+    }
+    
+    @IBAction func unwindSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
